d1ea9df035889c09c4478e82d346f1a7d2d0bbca
udonrm <b201395@hiroshima-u.ac.jp>
2024-03-14 17:09:38 +0900

    画像ファイルアップロード

--- src/app/api/group/route.ts
+++ src/app/api/group/route.ts
@@ -1,4 +1,8 @@
-import { NextResponse } from "next/server";
+import mime from "mime";
+import { join } from "path";
+import { stat, mkdir, writeFile } from "fs/promises";
+import { NextRequest, NextResponse } from "next/server";
+import _ from "lodash";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -27,11 +31,52 @@ export const GET = async () => {
 };
 
 export const POST = async (request: Request) => {
+  const formData = await request.formData();
+  const name = formData.get("name") as string;
+  const introduction = (formData.get("introduction") as string) || null;
+  const image = (formData.get("image") as File) || null;
+
+  const buffer = Buffer.from(await image.arrayBuffer());
+  const relativeUploadDir = `/uploads/${new Date(Date.now())
+    .toLocaleDateString("ja-JP", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    })
+    .replace(/\//g, "-")}`;
+
+  const uploadDir = join(process.cwd(), "public", relativeUploadDir);
+
   try {
-    const { name, image, introduction } = await request.json();
+    await stat(uploadDir);
+  } catch (e: any) {
+    if (e.code === "ENOENT") {
+      await mkdir(uploadDir, { recursive: true });
+    } else {
+      console.error(
+        "Error while trying to create directory when uploading a file\n",
+        e
+      );
+      return NextResponse.json(
+        { error: "Something went wrong." },
+        { status: 500 }
+      );
+    }
+  }
+
+  try {
+    // Math.random()で出力される0から1未満の数と1e9(1 * 10の9乗)の積でMath.round()で四捨五入
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    const filename = `${image.name.replace(
+      /\.[^/.]+$/,
+      ""
+    )}-${uniqueSuffix}.${mime.getExtension(image.type)}`;
+    await writeFile(`${uploadDir}/${filename}`, buffer);
+    const fileUrl = `${relativeUploadDir}/${filename}`;
+
     await main();
     const group = await prisma.group.create({
-      data: { name, image, introduction },
+      data: { name, image: fileUrl, introduction },
     });
     return NextResponse.json({ message: "success", group }, { status: 201 });
   } catch (e) {
