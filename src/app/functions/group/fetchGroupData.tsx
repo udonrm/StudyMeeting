04b4d97d1a6b41d58e995c0496c72a06954f14b0
Fkinds <Fkinds1020@gmail.com>
2024-03-29 16:31:42 +0900

    buge fix

--- src/app/functions/group/fetchGroupData.tsx
+++ src/app/functions/group/fetchGroupData.tsx
@@ -1,6 +1,7 @@
 export const fetchGroupData = async (id: number) => {
   const res = await fetch(`http://localhost:3000/api/group/${id}`, {
-    cache: "no-store",
+    // cache: "no-store",
+    next: { tags : ['groups/${id}'] },
   });
   const data = await res.json();
   return data.group;
